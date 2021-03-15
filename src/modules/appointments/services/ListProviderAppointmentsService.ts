edd0a5ccc6ac8a093042071423538ef94d997d32
João Pedro Rangel <jpmrangel22@gmail.com>
2021-03-15 15:25:59 -0300

    Cache/Redis added - WIP

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -2,8 +2,8 @@ import 'reflect-metadata';
 import { inject, injectable } from 'tsyringe';
 
 import Appointment from '../infra/typeorm/entities/Appointments';
-
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 interface IRequest {
     provider_id: string;
@@ -17,20 +17,31 @@ class ListProviderAppointmentsService {
     constructor(
         @inject('AppointmentsRepository')
         private appointmentsRepository: IAppointmentsRepository,
+
+        @inject('CacheProvider')
+        private cacheProvider: ICacheProvider,
     ) {}
 
-    public async execute({ 
-        provider_id, 
-        day, 
-        month, 
-        year 
+    public async execute({
+        provider_id,
+        day,
+        month,
+        year,
     }: IRequest): Promise<Appointment[]> {
-        const appointments = await this.appointmentsRepository.findAllInDayFromProvider({
-            provider_id,
-            day,
-            month,
-            year
-        });
+        const cachedData = await this.cacheProvider.recover('fon');
+
+        console.log(cachedData);
+
+        const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+            {
+                provider_id,
+                day,
+                month,
+                year,
+            },
+        );
+
+        // await this.cacheProvider.save('fon', 'trab');
 
         return appointments;
     }
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,32 +1,4 @@
-import { container } from 'tsyringe';
-import uploadConfig from '@config/upload';
-
-import IStorageProvider from './StorageProvider/models/IStorageProvider';
-import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
-import S3StorageProvider from './StorageProvider/implementations/S3StorageProvider';
-
-import IMailProvider from './MailProvider/models/IMailProvider';
-import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
-
-import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
-import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
-
-const storageProviders = {
-    disk: DiskStorageProvider,
-    s3: S3StorageProvider,
-};
-
-container.registerSingleton<IStorageProvider>(
-    'StorageProvider',
-    storageProviders[uploadConfig.driver],
-);
-
-container.registerSingleton<IMailTemplateProvider>(
-    'MailTemplateProvider',
-    HandlebarsMailTemplateProvider,
-);
-
-container.registerInstance<IMailProvider>(
-    'MailProvider',
-    container.resolve(EtherealMailProvider),
-);
+import './CacheProvider';
+import './MailProvider';
+import './MailTemplateProvider';
+import './StorageProvider';
