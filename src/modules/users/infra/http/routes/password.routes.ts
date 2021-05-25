1a656f2c1342ea821e808e7fc813650389dc0219
João Pedro Rangel <jpmrangel22@gmail.com>
2021-05-25 11:56:12 -0300

    Minors fixes in password services

--- src/modules/users/infra/http/routes/password.routes.ts
+++ src/modules/users/infra/http/routes/password.routes.ts
@@ -22,6 +22,7 @@ passwordRouter.post(
     celebrate({
         [Segments.BODY]: {
             token: Joi.string().uuid().required(),
+            password: Joi.string().required(),
             password_confirmation: Joi.string()
                 .required()
                 .valid(Joi.ref('password')),
